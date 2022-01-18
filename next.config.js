15f73811e1368fb6ab61132842a7a287644dd257
Blind Ganks <abdelhaketabk@gmail.com>
2022-01-18 14:12:02 +0100

    added Stripe checkout

--- next.config.js
+++ next.config.js
@@ -6,4 +6,7 @@ module.exports = {
       "fakestoreapi.com",
     ],
   },
+  env: {
+    stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+  },
 };
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -4,13 +4,31 @@ import { useSelector } from "react-redux";
 import CheckoutProduct from "../components/CheckoutProduct";
 import Header from "../components/Header";
 import { selectItems, selectTotal } from "../slices/basketSlice";
+import { loadStripe } from "@stripe/stripe-js";
+const axios = require("axios");
 
 function checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const currencyFormatter = require("currency-formatter");
-  const session = useSession();
+  const { data: session } = useSession();
+  const stripePromise = loadStripe(process.env.stripe_public_key);
 
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    //call the backend to create a checkout session
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items,
+      email: session.user.email,
+    });
+    //redirect user to stripe checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (result.error) alert(result.error.message);
+  };
   return (
     <div className="bg-gray-100">
       <Header />
@@ -57,6 +75,8 @@ function checkout() {
                 </span>
               </h2>
               <button
+                role="link"
+                onClick={createCheckoutSession}
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
