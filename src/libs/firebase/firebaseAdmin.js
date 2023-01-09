2419eb4e4eb505036320902ef68bfd78c0109500
blindganks <abdelhaktebak@gmail.com>
2023-01-09 13:33:10 +0100

    this works for sure

--- src/libs/firebase/firebaseAdmin.js
+++ src/libs/firebase/firebaseAdmin.js
@@ -1,20 +1,19 @@
-import { initializeApp } from "firebase-admin";
-import { getApp, getApps } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
 import { firebaseServiceAccount } from "./firebase-creds";
-let app = !getApps().length
-  ? initializeApp({
-      credential: admin.credential.cert(
-        {
+import * as admin from "firebase-admin";
+
+let app = !admin.apps.length
+  ? admin.initializeApp(
+      {
+        credential: admin.credential.cert({
           privateKey: firebaseServiceAccount.private_key.replace(/\\n/g, "\n"),
           clientEmail: firebaseServiceAccount.client_email,
           projectId: firebaseServiceAccount.project_id,
-        },
-        "admin-app"
-      ),
-    })
-  : getApp("admin-app");
+        }),
+      },
+      "admin-app"
+    )
+  : admin.app("admin-app");
 
-const adminFirestore = getFirestore(app);
+const adminFirestore = admin.firestore(app);
 
 export default adminFirestore;
